4bc050452a0a0e336e3a5f28d9ed6b615d7ee97e
Sergey Rychkov <rychkovwlk@gmail.com>
2022-08-27 01:44:17 +0300

    update task2 for bash

--- Bash/task2.sh
+++ Bash/task2.sh
@@ -17,7 +17,7 @@ fi
 
 awk '{
     if ( index($0, "[")==1 ){                                                                 # first line
-       nameSample = substr($0,index($0, "[") + 1, (index($0, "]")-(index($0, "[")+1)))
+       nameSample = substr($0,index($0,"[") + 2,(index($0, "]")-(index($0,"[")+3)))
        numberOfTests = substr($0, index($0,"..")+2, index($0,"tests") - index($0,"..") -3)    # get number of test 
        printf ("{" "\n")
        printf ("\"testname\": \""nameSample"\"," "\n")
@@ -43,27 +43,29 @@ awk '{
             status = "true"      # set if test passed or not
         printf ("{" "\n")
         printf ("\"name\": \""name"\"," "\n")
-        printf ("\"status\": \""status"\"," "\n")
+        printf ("\"status\": "status"," "\n")
         printf ("\"duration\": \""duration"\"" "\n")
         if(NR==numberOfTests+2)
             printf ("}" "\n")     # if last number of test we did not wrote a comma
         else 
             printf ("}," "\n")
     }
-} END{           # last line
+}END{           # last line
         
         printf ("\"summary\": {" "\n")
         success = substr($0, 0,match($0, /^[0-9]+/))                                       # get first digit from string
         failed = substr($0, 
-                        match($0, /[0-9]+ tests failed/), 
-                        match($0, /tests failed/)- match($0, /[0-9]+ tests failed/))       # find test failed number
+                        match($0, /[0-9]+ tests failed/),\ 
+                        match($0, /tests failed/) - match($0,/[0-9]+ tests failed/) - 1 )       # find test failed number
+       
         rating = substr($0,
-                        match($0, /rated as/) +9,
+                        match($0, /rated as/) +9,\
                         match($0, /%/) - match($0, /rated/) - 9 )                        # find rating without % symbol
-        durationALL = substr($0, match($0, /spent/)+6, length($0)) 
-        printf ("\"success\": \""success"\","  "\n")
-        printf ("\"failed\": \""failed"\","  "\n")
-        printf ("\"rating\": \""rating"\","  "\n")
+                        
+        durationALL = substr($0, match($0,/spent/)+6, length($0) - 2)
+        printf ("\"success\": "success","  "\n")
+        printf ("\"failed\": "failed"," "\n")
+        printf ("\"rating\": "rating","  "\n")
         printf ("\"duration\": \""durationALL"\""  "\n")
         printf ("}" "\n")
         printf ("}" "\n")    # end of file
